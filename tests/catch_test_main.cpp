b1b35757a67dfcd14c8ac95abf460dae0f98def7
Darius Arnold <mail@dariusarnold.de>
2021-03-21 03:33:31 +0100

    Add tests for clear and size

--- tests/catch_test_main.cpp
+++ tests/catch_test_main.cpp
@@ -3,6 +3,55 @@
 #include "ringbuffer.hpp"
 
 
+TEST_CASE("Test capacity and size from pushing") {
+    ringbuffer<double, 5> a;
+    CHECK(a.capacity() == 5);
+    CHECK(a.empty());
+    CHECK(a.size() == 0);
+
+    SECTION("Pushing increases size") {
+        a.push_back(1);
+        CHECK(a.size() == 1);
+        CHECK_FALSE(a.empty());
+        a.push_back(2.5);
+        CHECK(a.size() == 2);
+    }
+
+    SECTION("Push when at capacity doesn't increase size anymore") {
+        for (int i = 0; i < a.capacity(); ++i) {
+            a.push_back(0);
+        }
+        CHECK(a.size() == 5);
+        a.push_back(1);
+        CHECK(a.size() == 0);
+    }
+}
+
+
+TEST_CASE("Test capacity and size of prefilled buffer") {
+    ringbuffer<int, 4> buff_full{1, 2, 3, 4};
+    CHECK(buff_full.size() == 4);
+    ringbuffer<int, 4> buff_half{1, 2};
+    CHECK(buff_half.size() == 2);
+    ringbuffer<int, 3> prefilled{7};
+    CHECK(prefilled.size() == 7);
+}
+
+
+TEST_CASE("Reset clears buffer") {
+    ringbuffer<int, 3> buff;
+    buff.push_back(2);
+    buff.clear();
+    CHECK(buff.size() == 0);
+    buff.push_back(10);
+    buff.push_back(20);
+    buff.push_back(30);
+    CHECK(buff[0] == 10);
+    CHECK(buff[1] == 20);
+    CHECK(buff[2] == 30);
+}
+
+
 TEST_CASE("Test filling ringbuffer to capacity") {
     ringbuffer<int, 3> buff;
     buff.push_back(1);
